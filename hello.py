e8bd78b2179e31a39ba975925fde970557b1a487
ElizabethJMartinezA <111543644+ElizabethJMartinezA@users.noreply.github.com>
2022-12-15 16:49:38 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 #create a Flask Instance
 app=Flask(__name__)
@@ -6,7 +7,32 @@ app=Flask(__name__)
 #create a route decorator
 @app.route('/')
 def index():
-    return render_template("index.html")
+    
+    url = "http://127.0.0.1:5000/listImages"
+    images = requests.get(url=url).json()
+    
+    url2 = "http://127.0.0.1:5000/flavoursList"
+    flavours = requests.get(url=url2).json()
+    
+    url3 = "http://127.0.0.1:5000/keypairsList"
+    keyPairs = requests.get(url=url3).json()
+    
+    url4 = "http://127.0.0.1:5000/securityGroups"
+    sGropus = requests.get(url=url4).json()
+    
+    url5 = "http://127.0.0.1:5000/networksList"
+    networks = requests.get(url=url5).json()
+    
+    #url6 = "http://127.0.0.1:5000/getJson"
+        
+    return render_template("index.html", images=images,flavours=flavours, keyPairs=keyPairs, sGropus=sGropus,networks=networks)
 # virtualenv web
 #source bin/activate
 #flask --app hello.py --debug run
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8002)
+    
+    #export FLASK_APP=hello.py
+    #export FLASK_ENV=development
+    #flask run
